5bdafacf5a1e78ef2a6d86fc52525761a8b2418d
Ritwik Dev <43510943+RitwikDev@users.noreply.github.com>
2023-09-18 18:48:37 -0500

    Adding constant for TTL

--- Assets/Scripts/2D/Weapon/Bullet.cs
+++ Assets/Scripts/2D/Weapon/Bullet.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     const int SPEED = 15;
+    const int TTL_SECONDS = 10;
 
     public bool exists;
 
     public void Start()
     {
         exists = true;
-        Destroy(gameObject, 5);
+        Destroy(gameObject, TTL_SECONDS);
     }
 
     public void AddForce(Vector2 direction)
